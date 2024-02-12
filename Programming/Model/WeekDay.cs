5aa43934c719dd8d8ec937c1af1a9f43237aa985
MonkeHlam <vecktor887@gmail.com>
2024-02-12 14:31:12 +0700

    Create new project "Programming" with enumeration WeekDay

---
+++ Programming/Model/WeekDay.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Programming.Model
+{
+    enum WeekDay
+    {
+        Monday,
+        Tuesday,
+        Wednesday,
+        Thursday,
+        Friday,
+        Saturday,
+        Sunday
+    }
+}
