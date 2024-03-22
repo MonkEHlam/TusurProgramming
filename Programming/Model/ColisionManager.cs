3e1f3ae7a74a565fccbbe05b1fac91bac4e880c2
MonkeHlam <vecktor887@gmail.com>
2024-03-22 12:28:30 +0700

    Lab 4 ended

---
+++ Programming/Model/ColisionManager.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+
+namespace Programming.Model
+{
+    internal static class ColisionManager
+    {
+        public static bool IsColision(Rectangle rectangle1, Rectangle rectangle2)
+        {
+            bool xColision = Math.Abs(rectangle1.Center.X - rectangle2.Center.X) < Math.Abs(rectangle1.Width - rectangle2.Width) / 2;
+            bool yColision = Math.Abs(rectangle1.Center.Y - rectangle2.Center.Y) < Math.Abs(rectangle1.Length - rectangle2.Length) / 2;
+            return xColision && yColision;
+        }
+
+        public static bool IsColision(Ring ring1, Ring ring2) 
+        {
+            double c = Math.Pow(Math.Pow
+                (
+                Math.Abs(ring1.Center.X - ring2.Center.X), 2) +
+                Math.Pow(Math.Abs(ring1.Center.Y - ring2.Center.Y), 2
+                ), 0.5);
+            return c < ring1.ExternalRadius + ring2.ExternalRadius;
+        }
+    }
+}
