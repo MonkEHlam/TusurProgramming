05549caccae6cb3b813fd8cd80fc5334d427c2f2
MonkeHlam <vecktor887@gmail.com>
2024-11-18 20:28:29 +0700

    Add priority order class

---
+++ ObjectOrientedPractice/ObjectOrientedPractice/Model/DeliveryTime.cs
@@ -0,0 +1,15 @@
+﻿namespace ObjectOrientedPractice.Model
+{
+    /// <summary>
+    /// Time of delivery for <see cref="PriorityOrder">priority orders</see>.
+    /// </summary>
+    enum DeliveryTime
+    {
+        NineToEleven,
+        ElevenToThirteen, 
+        ThirteenToFifteen,
+        FifteenToSeventeen,
+        SeventeenToNineteen,
+        NineteenToTwentyOne
+    }
+}
---
+++ ObjectOrientedPractice/ObjectOrientedPractice/Model/PriorityOrder.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ObjectOrientedPractice.Model
+{
+    /// <summary>
+    /// Represents a priorite order.
+    /// Priority order contains additional info about date and time of delivery.
+    /// </summary>
+    internal class PriorityOrder : Order
+    {
+        /// <summary>
+        /// Date of delivery.
+        /// </summary>
+        DateTime DeliveryDay { get; set; }
+
+        /// <summary>
+        /// Time of delivery.
+        /// </summary>
+        DeliveryTime DeliveryTime { get; set; } 
+
+        /// <summary>
+        /// Base class constructor.
+        /// </summary>
+        /// <param name="items"></param>
+        /// <param name="amount"></param>
+        /// <param name="address"></param>
+        public PriorityOrder(List<Item> items, double amount, Address address)
+            : base(items, amount, address)
+        {
+            DeliveryDay = DateTime.Today;
+            DeliveryTime = DeliveryTime.NineToEleven;
+        }
+    }
+}
