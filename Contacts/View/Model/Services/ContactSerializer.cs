e97fefedf1f69a8fd6b64e6f93cd4b20bd28b085
MonkeHlam <vecktor887@gmail.com>
2025-03-10 23:05:37 +0700

    Initialize project, complete UI/UX, Complete Model classes

---
+++ Contacts/View/Model/Services/ContactSerializer.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace View.Model.Services
+{
+    internal class ContactSerializer
+    {
+        /// <summary>
+        /// Path to json save file.
+        /// </summary>
+        public string FilePath;
+
+        /// <summary>
+        /// Base class constructor.
+        /// </summary>
+        /// <param name="filePath">Path for save file. MyDocuments/Contacts by default.</param>
+        public ContactSerializer(string filePath = null)
+        {
+            if (filePath == null)
+            {
+                FilePath = Path.Combine(
+                Environment.GetFolderPath(
+                    Environment.SpecialFolder.MyDocuments),
+                "Contacts",
+                "contact.json");
+            }
+            else
+            {
+                FilePath = filePath;
+            }
+
+            Directory.CreateDirectory(FilePath);
+        }
+
+        /// <summary>
+        /// Save <see cref="Contact"/> entity into json file.
+        /// </summary>
+        /// <param name="contact">Contact entity for saving</param>
+        void Serialize(Contact contact)
+        {
+            if (contact == null) { return; }
+            
+            try
+            {
+                string json = JsonConvert.SerializeObject(contact, Formatting.Indented);
+                File.WriteAllText(json, FilePath);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error on content saving: " + ex.Message);
+            }
+        }
+
+        /// <summary>
+        /// Load saved <see cref="Contact"/> from json file.
+        /// </summary>
+        /// <returns></returns>
+        Contact Deserialize()
+        {
+            if (!File.Exists(FilePath)) { return null; }
+            try
+            {
+                Contact contact = JsonConvert.DeserializeObject<Contact>(File.ReadAllText(FilePath));
+                return contact;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error on content load: " + ex.Message);
+                return null;
+            }
+        }
+    }
+}
---
+++ Contacts/View/ViewModel/MainVM.cs
@@ -0,0 +1,82 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using View.Model;
+
+namespace View.ViewModel
+{
+    internal class MainVM : INotifyPropertyChanged
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        private Contact _contact;
+        
+        /// <summary>
+        /// 
+        /// </summary>
+        private string _name;
+        
+        /// <summary>
+        /// 
+        /// </summary>
+        private string _email;
+        
+        /// <summary>
+        /// 
+        /// </summary>
+        private string _phone;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public string Name
+        {
+            get { return _name; }
+            set 
+            {
+                _name = value;
+                OnPropertyChanged("Name");
+            }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public string Email
+        {
+            get { return _email; }
+            set
+            {
+                _email = value; 
+                OnPropertyChanged("Email");
+            }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public string Phone
+        {
+            get { return _phone; }
+            set
+            {
+                _phone = value;
+                OnPropertyChanged("Phone");
+            }
+        }
+
+        /// <summary>
+        /// <inheritdoc/>
+        /// </summary>
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="propertyName"></param>
+        public void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
